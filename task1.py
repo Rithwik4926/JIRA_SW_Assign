b143e3c25038e892ad78a5d3beb078c7351218e2
Rithwik Pathuri <pathuririthwik@gmail.com>
2025-09-01 23:22:13 +0530

    SCRUM-6: Updated task1.py with new lines of code

--- task1.py
+++ task1.py
@@ -4,3 +4,17 @@ def generate_question():
     a = random.randint(2, 12)
     b = random.randint(2, 12)
     return a, b, a * b
+
+def ask_question(num):
+    a, b, correct_ans = generate_question()
+    print(f"Question {num}: {a} x {b} = ?")
+    try:
+        ans = int(input("Your answer: "))
+    except ValueError:
+        ans = None
+    if ans == correct_ans:
+        print("Right!")
+        return True
+    else:
+        print(f"Wrong. The answer is {correct_ans}.")
+        return False
